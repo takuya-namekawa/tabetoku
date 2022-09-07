ea2a08dbf53dd0901e9a7a57a4a2fa67613e026f
takuya-namekawa <tatatatatata89@gmail.com>
2022-09-07 19:39:43 +0000

    ショップ側予約一覧ページ

--- app/controllers/admin/reserves_controller.rb
+++ app/controllers/admin/reserves_controller.rb
@@ -1,12 +1,13 @@
 class Admin::ReservesController < ApplicationController
   def index
-   
+
     @reserves = current_admin.reserves.all
+    # @order = Order.customer.find_by(customer_id: customer_id)
   end
 
   def show
     @reserve = current_admin.reserves.find(params[:id])
   end
-  
-  
+
+
 end
