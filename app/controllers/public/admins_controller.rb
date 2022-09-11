98460f82f8de4a26d9a29c5869cfa14baed24005
takuya-namekawa <tatatatatata89@gmail.com>
2022-09-11 19:54:26 +0000

    ショップ詳細ページ 一覧ページ更新

--- app/controllers/public/admins_controller.rb
+++ app/controllers/public/admins_controller.rb
@@ -1,4 +1,6 @@
 class Public::AdminsController < ApplicationController
+  before_action :set_search, only: [:index, :search]
+
   def index
     @admins = Admin.all
     @genres = Genre.all
@@ -14,7 +16,15 @@ class Public::AdminsController < ApplicationController
     @admin = Admin.find(params[:id])
   end
 
+  def search
+    @results = @search.result
+  end
+
   private
+
+  def set_search
+    @search = Item.ransack(params[:q])
+  end
 
   def event_params
      params.require(:event).permit(:title, :content, :address, :latitude, :longitude)
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,15 +1,26 @@
 class Public::HomesController < ApplicationController
+  before_action :set_search, only: [:index, :search, :top]
 
   def top
     @items = Item.all.order(created_at: :desc)
     @genres = Genre.all
     @admins = Admin.all
   end
-  
+
   def genre_items
     @genres = Genre.all
     @genre = Genre.find(params[:item_id])
     @genre_items = @genre.items.all
-  end  
+  end
+
+  def search
+    @results = @search.result
+  end
+
+  private
+
+  def set_search
+    @search = Item.ransack(params[:q])
+  end
 
 end
