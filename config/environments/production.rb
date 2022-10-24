eae11a5aa0365b7dae077147a39f234b5731cd0f
takuya-namekawa <tatatatatata89@gmail.com>
2022-10-24 10:31:13 +0000

    アクションメーラー　本番設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name: ENV["EMAIL_ADDRESS"],
+      password: ENV["EMAIL_SECRET_KEY"],
+      domain: "gmail.com",
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 end
